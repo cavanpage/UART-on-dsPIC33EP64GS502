a05b122576876a3604aac80adb881959a919b835
cavanpage <cavan100@gmail.com>
2016-03-25 01:02:57 -0700

    successfull sending strings from dsPIC to PC

--- main.c
+++ main.c
@@ -58,19 +58,17 @@
 #pragma config IBSEQ = 0xFFF            // The one's complement of BSEQ; must be calculated by the user and written during device programming. (Inverse Boot Sequence Number bits)
 
 
-int counter;
+unsigned long counter;
+char membuffer[2];
 
 void InitUART1( void )
 {
- // configure U1MODE
  U1MODEbits.UARTEN = 0; // Bit15 TX, RX DISABLED, ENABLE at end of func
 
- //U1MODEbits.notimplemented;// Bit14
  U1MODEbits.USIDL = 0; // Bit13 Continue in Idle
  U1MODEbits.IREN = 0; // Bit12 No IR translation
  U1MODEbits.RTSMD = 0; // Bit11 Simplex Mode
 
- //U1MODEbits.notimplemented;// Bit10
  U1MODEbits.UEN = 0; // Bits8,9 TX,RX enabled, CTS,RTS not
  U1MODEbits.WAKE = 0; // Bit7 No Wake up (since we don't sleep here)
  U1MODEbits.LPBACK = 0; // Bit6 No Loop Back
@@ -79,46 +77,74 @@ void InitUART1( void )
  U1MODEbits.PDSEL = 0; // Bits1,2 8bit, No Parity
  U1MODEbits.STSEL = 0; // Bit0 One Stop Bit
 
- // Load a value into Baud Rate Generator. 
  U1BRG = UBRG1_VALUE; // 60Mhz osc, 9600 Baud
 
- // Load all values in for U1STA SFR
  U1STAbits.UTXISEL1 = 0; //Bit15 Int when Char is transferred (1/2 config!)
  U1STAbits.UTXINV = 0; //Bit14 N/A, IRDA config
  U1STAbits.UTXISEL0 = 0; //Bit13 Other half of Bit15
 
- //U1STAbits.notimplemented = 0;//Bit12
  U1STAbits.UTXBRK = 0; //Bit11 Disabled
  U1STAbits.UTXEN = 0; //Bit10 TX pins controlled by periph
- //U1STAbits.UTXBF //Bit9 *Read Only Bit*
- //U1STAbits.TRMT //Bit8 *Read Only bit*
+
  U1STAbits.URXISEL = 0; //Bits6,7 Int. on character recieved
  U1STAbits.ADDEN = 0; //Bit5 Address Detect Disabled
- //U1STAbits.RIDLE //Bit4 *Read Only Bit*
- //U1STAbits.PERR //Bit3 *Read Only Bit*
- //U1STAbits.FERR //Bit2 *Read Only Bit*
- //U1STAbits.OERR //Bit1 *Read Only Bit*
- //U1STAbits.URXDA //Bit0 *Read Only Bit*
-// IPC7 = 0x4400; // Mid Range Interrupt Priority level, no urgent reason
-// IFS0bits.U1TXIF = 0; // Clear the Transmit Interrupt Flag
-// IEC0bits.U1TXIE = 1; // Enable Transmit Interrupts
-// IFS0bits.U1RXIF = 0; // Clear the Recieve Interrupt Flag
-// IEC0bits.U1RXIE = 1; // Enable Recieve Interrupts
  
  U1MODEbits.UARTEN = 1; // And turn the peripheral on
  U1STAbits.UTXEN = 1;
 }
 
+void request_packet(void) 
+{
+    U1STAbits.URXDA = 0;
+    USART_puts("RTS"); //send request to send message
+    
+    if(U1STAbits.URXDA == 1)
+    { 
+        LATAbits.LATA0   = 1;
+        membuffer[0] = U1RXREG;  
+        membuffer[1] = U1RXREG;
+                                                                                  
+        U1STAbits.URXDA = 0;
+     }
+    else LATAbits.LATA0 = 0; 
+    
+    unsigned long j = 0;
+    while(j++ < 200000){
+       
+    }
+    LATAbits.LATA0   = 0;
+    
+}
+
+void send_packet(void){
+   USART_puts("SND");
+   USART_puts("HEADERXX");
+   USART_puts("BODYXXXX");
+   USART_puts("CHECKSUM");
+}
+
+void USART_putc(unsigned char c)
+{
+    while (!U1STAbits.TRMT); // wait until transmit shift register is empty
+    U1TXREG = c;               // write character to TXREG and start transmission
+}
+void USART_puts(unsigned char *s)
+{
+    while (*s)
+    {
+        USART_putc(*s);     // send character pointed to by s
+        s++;                // increase pointer location to the next character
+    }
+}
+
 int main() {
     
         RCONbits.SWDTEN = 0; //Disable Watch Dog Timer
-
-        
+      
         OSCTUN = 0; //center frequency ~ 7.373MHz
         TRISAbits.TRISA0 = 0; // Set RA0 as an output for LED
         LATAbits.LATA0 = 1; // Turn on the LED
         
-
         //RPOR0bits.RP32R = 1; //set pin 5(RP32) as Tx
         RPOR1bits.RP35R = 1; //pin pin 11(RP35) as TX
         RPINR18bits.U1RXR = 36; //set pin 12(RP36) as Rx
@@ -133,40 +159,19 @@ int main() {
         
         counter = 0;
         
-        U1TXREG  = 'C';
+       // U1TXREG  = 'C';
         
        // IFS0bits.U1TXIF = 0;
-        while(1){
-            
-            if(counter > 30000)
-            {
-                U1TXREG = 255;
-                
-                if(U1STAbits.OERR ==1)
-                {
-                    U1STAbits.OERR = 0;
-                    continue;
-                }
-                
-                
-                //check if receive buffer has data 
-                if(U1STAbits.URXDA == 1)
-                { 
-                    LATAbits.LATA0   = 1;
-                    
-                    
-                    char membuffer = U1RXREG;
-                                
-                    U1STAbits.URXDA = 0;
-                }
-           else  LATAbits.LATA0   = ~LATAbits.LATA0;
-                
-                counter= 0;
-                //IFS0bits.U1TXIF = 0; //clear transmit flag
-               
-            }
-            else counter++;
-           
+
+        
+        while(1)
+        {
+            if(counter++ > 200000)
+            {     
+                request_packet();
+                send_packet();
+                counter = 0;
+            }         
         }
         
         return 1; 
